c2cc30e2e61530086aea43c560f8e53f75d21a3c
Leonardo Serrano <vageyenaman@gmail.com>
2019-08-14 19:00:21 -0700

    Adds a new test for satellites

--- src/kepler_orbit.test.cpp
+++ src/kepler_orbit.test.cpp
@@ -13,7 +13,6 @@ using json = nlohmann::json;
  * 	Test to see if the planet Earth (using orbital parameters mostly taken from Wikipedia) can be simulated using KeplerOrbit logic
  */
 TEST_CASE("Earth can be properly simulated using KeplerOrbit logic") {
-
 	std::ifstream i("data/sol.json");
 	json sol;
 	i >> sol;
@@ -31,8 +30,8 @@ TEST_CASE("Earth can be properly simulated using KeplerOrbit logic") {
 		CHECK(earthOrbit.getApoapsis() == doctest::Approx(152097596.5271).epsilon(0.0001));
 		CHECK(earthOrbit.getSemiminorAxis() == doctest::Approx(149577139.3552).epsilon(0.0001));
 		CHECK(earthOrbit.getLPeriapsis() == doctest::Approx(1.7967674211761813).epsilon(0.0001));
-		CHECK(earthOrbit.getMeanAngularMotion() == doctest::Approx(-1.9910e-7).epsilon(0.0001));
-		CHECK(earthOrbit.getPeriod() == doctest::Approx(1).epsilon(0.0001));
+		CHECK(earthOrbit.getMeanAngularMotion() == doctest::Approx(-1.9910e-7).epsilon(1e-10));
+		CHECK(earthOrbit.getPeriod() == doctest::Approx(1).epsilon(1e-7));
 	}
 	SUBCASE("test Earth's orbital center") {
 		Vec2 center = earthOrbit.getCenter(solarPosition);
@@ -71,13 +70,45 @@ TEST_CASE("Earth can be properly simulated using KeplerOrbit logic") {
 /**
  * 	Test to see if the Moon can be simulated using KeplerOrbit logic
  */
-// TEST_CASE("The Moon can be properly simulated using KeplerOrbit logic") {
-// 	double solarMass = 1.9885e30; // solar mass in kg
-// 	Vec2 solarPosition = {0, 0};
+TEST_CASE("The Moon can be properly simulated using KeplerOrbit logic") {
+	std::ifstream i("data/sol.json");
+	json sol;
+	i >> sol;
 
-// 	double earthMass = 5.97237e24; // earth mass in kg
-// 	double standardGravTotal = (solarMass * Util::GRAV) + (earthMass * Util::GRAV);
-// 	KeplerOrbit earthOrbit(149598023.0, 0.0167086, 0.0, 6.259047404, -0.196535244, 1.99330266505, standardGravTotal, true);
+	double solarMass = sol["mass"]; // solar mass in kg
+	Vec2 solarPosition = {0, 0};
 
-// 	// double lunarMass = 
-// }
+	json earth = sol["children"][2];
+	double earthMass = earth["mass"]; // earth mass in kg
+	double standardGravTotal = (solarMass * Util::GRAV) + (earthMass * Util::GRAV);
+	KeplerOrbit earthOrbit(earth["orbit"]["semimajorAxis"], earth["orbit"]["eccentricity"], earth["orbit"]["epochTime"], earth["orbit"]["epochAnomaly"], earth["orbit"]["lAscending"], earth["orbit"]["aPeriapsis"], standardGravTotal, true);
+
+	json moon = earth["children"][0];
+	double lunarMass = moon["mass"];
+	double standardGravTotalMoon = (earthMass * Util::GRAV) + (lunarMass * Util::GRAV);
+	KeplerOrbit moonOrbit(moon["orbit"]["semimajorAxis"], moon["orbit"]["eccentricity"], moon["orbit"]["epochTime"], moon["orbit"]["epochAnomaly"], moon["orbit"]["lAscending"], moon["orbit"]["aPeriapsis"], standardGravTotalMoon, true);
+
+
+	SUBCASE("test Moon's computed static orbital elements") {
+		CHECK(moonOrbit.getPeriapsis() == doctest::Approx(363295.4949).epsilon(0.0001));
+		CHECK(moonOrbit.getApoapsis() == doctest::Approx(405502.5051).epsilon(0.0001));
+		CHECK(moonOrbit.getSemiminorAxis() == doctest::Approx(383819.2716).epsilon(0.0001));
+		CHECK(moonOrbit.getLPeriapsis() == doctest::Approx(7.735822843614466).epsilon(0.0001));
+		CHECK(moonOrbit.getMeanAngularMotion() == doctest::Approx(-0.0000026653).epsilon(1e-10));
+		CHECK(moonOrbit.getPeriod() == doctest::Approx(0.0001302).epsilon(1e-7));
+	}
+	SUBCASE("test conditions at t = 0") {
+		universeTime t = 0.0;
+		SUBCASE("test Moon's anomaly computations") {
+			CHECK(moonOrbit.computeMeanAnomaly(t) == doctest::Approx(-2.2149).epsilon(0.0001));
+			CHECK(moonOrbit.computeEccentricAnomaly(moonOrbit.computeMeanAnomaly(t)) == doctest::Approx(-2.25743).epsilon(0.0001));
+			CHECK(moonOrbit.computeTrueAnomaly(t) == doctest::Approx(-2.2991).epsilon(0.0001));
+		}
+		SUBCASE("test Moon's position (relative to the Sun)") {
+			Vec2 earthPosition = earthOrbit.computeCoordinates(solarPosition, t);
+			Vec2 position = moonOrbit.computeCoordinates(earthPosition, t);
+			CHECK(position.x == doctest::Approx(-29695629.5249).epsilon(0.0001));
+			CHECK(position.y == doctest::Approx(-143909480.6461).epsilon(0.0001));
+		}
+	}
+}
