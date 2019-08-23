5979e0133abd25a843ded5d6d59a2b24a581a282
Leonardo Serrano <vageyenaman@gmail.com>
2019-08-22 21:00:32 -0700

    Fixes more spacing

--- external/imgui/imgui_draw.cpp
+++ external/imgui/imgui_draw.cpp
@@ -895,19 +895,22 @@ void ImDrawList::PathArcTo(const ImVec2& centre, float radius, float a_min, floa
 
 void ImDrawList::PathEllipticalArcTo(const ImVec2& center, float radius_x, float radius_y, float rot, float a_min, float a_max, int num_segments)
 {
-	_Path.reserve(_Path.Size + (num_segments + 1));
-	for(int i = 0; i <= num_segments; i++)
-	{
-		const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
-		ImVec2 point(center.x + ImCos(a) * radius_x, center.y + ImSin(a) * radius_y);
-		point.x -= center.x;
-		point.y -= center.y;
-		float rel_x = (point.x * ImCos(rot)) - (point.y * ImSin(rot));
-		float rel_y = (point.x * ImSin(rot)) + (point.y * ImCos(rot));
-		point.x = rel_x + center.x;
-		point.y = rel_y + center.y;
-		_Path.push_back(point);
-	}
+    _Path.reserve(_Path.Size + (num_segments + 1));
+
+    const float cosRot = ImCos(rot);
+    const float sinRot = ImSin(rot);
+    for(int i = 0; i <= num_segments; i++)
+    {
+        const float a = a_min + ((float)i / (float)num_segments) * (a_max - a_min);
+        ImVec2 point(center.x + ImCos(a) * radius_x, center.y + ImSin(a) * radius_y);
+        point.x -= center.x;
+        point.y -= center.y;
+        const float rel_x = (point.x * cosRot) - (point.y * sinRot);
+        const float rel_y = (point.x * sinRot) + (point.y * cosRot);
+        point.x = rel_x + center.x;
+        point.y = rel_y + center.y;
+        _Path.push_back(point);
+    }
 }
 
 static void PathBezierToCasteljau(ImVector<ImVec2>* path, float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4, float tess_tol, int level)
@@ -1107,24 +1110,24 @@ void ImDrawList::AddCircleFilled(const ImVec2& centre, float radius, ImU32 col, 
 
 void ImDrawList::AddEllipse(const ImVec2& center, float radius_x, float radius_y, ImU32 col, float rot, int num_segments, float thickness)
 {
-	if ((col & IM_COL32_A_MASK) == 0 || num_segments <= 2)
-		return;
+    if ((col & IM_COL32_A_MASK) == 0 || num_segments <= 2)
+        return;
 
-	// Because we are filling a closed shape we remove 1 from the count of segments/points
-	const float a_max = IM_PI * 2.0f * ((float)num_segments - 1.0f) / (float)num_segments;
-	PathEllipticalArcTo(center, radius_x, radius_y, rot, 0.0f, a_max, num_segments - 1);
-	PathStroke(col, true, thickness);
+    // Because we are filling a closed shape we remove 1 from the count of segments/points
+    const float a_max = IM_PI * 2.0f * ((float)num_segments - 1.0f) / (float)num_segments;
+    PathEllipticalArcTo(center, radius_x, radius_y, rot, 0.0f, a_max, num_segments - 1);
+    PathStroke(col, true, thickness);
 }
 
 void ImDrawList::AddEllipseFilled(const ImVec2& center, float radius_x, float radius_y, ImU32 col, float rot, int num_segments)
 {
-	if((col & IM_COL32_A_MASK) == 0 || num_segments <= 2)
-		return;
+    if((col & IM_COL32_A_MASK) == 0 || num_segments <= 2)
+        return;
 
-	// Because we are filling a closed shape we remove 1 from the count of segments/points
-	const float a_max = IM_PI * 2.0f * ((float)num_segments - 1.0f) / (float)num_segments;
-	PathEllipticalArcTo(center, radius_x, radius_y, rot, 0.0f, a_max, num_segments - 1);
-	PathFillConvex(col);
+    // Because we are filling a closed shape we remove 1 from the count of segments/points
+    const float a_max = IM_PI * 2.0f * ((float)num_segments - 1.0f) / (float)num_segments;
+    PathEllipticalArcTo(center, radius_x, radius_y, rot, 0.0f, a_max, num_segments - 1);
+    PathFillConvex(col);
 }
 
 void ImDrawList::AddBezierCurve(const ImVec2& pos0, const ImVec2& cp0, const ImVec2& cp1, const ImVec2& pos1, ImU32 col, float thickness, int num_segments)
